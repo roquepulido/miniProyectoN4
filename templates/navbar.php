bd7362c50d0df12e8d02f8010b65b64edcea2297
Roque Pulido <roque.pulido@outlook.com>
2023-01-24 12:16:05 -0500

    nav template

---
+++ templates/navbar.php
@@ -0,0 +1,62 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand"><img class="logodev" src="./img/devchallenges.svg" alt="logoDev" /></a>
+
+        <div class="left__nav d-flex align-items-center gap-2">
+
+            <img class="border rounded-4 avatar" src="./img/Users/Chuck_Norris.jpg" alt="User Img">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Chuck Norris
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="px-1"><a class="dropdown-item" href="#">My Profile</a></li>
+                        <li class="px-1"><a class="dropdown-item" href="#">Group Chat</a></li>
+                        <li class="px-3">
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li class="px-1"><a class="dropdown-item" href="#">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
+
+<!-- <nav class="navbar  bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img class="logodev" src="./img/devchallenges.svg" alt="logoDev" /></a>
+        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <img class="border rounded-4 avatar" src="./img/Users/Chuck_Norris.jpg" alt="User Img"><span
+                class="navbar__name">Chuck Norris</span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Dropdown link
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav> -->
--- user.php
+++ user.php
@@ -1,38 +1,8 @@
 <?php
 include("./templates/headhtml.php");
+include("./templates/navbar.php");
 ?>
-<nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid d-flex aling-items-center justify-content-space ms-5">
-        <a class="navbar-brand" href="#"><img class="logodev" src="./img/devchallenges.svg" alt="logoDev" /></a>
 
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <img class="border rounded-4 avatar" src="./img/Users/Chuck_Norris.jpg" alt="User Img">
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Chuck Norris
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="px-1"><a class="dropdown-item" href="#">My Profile</a></li>
-                        <li class="px-1"><a class="dropdown-item" href="#">Group Chat</a></li>
-                        <li class="px-3">
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li class="px-1"><a class="dropdown-item color-danger" href="#">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
 <?php
 include("./templates/footer.php");
 ?>
